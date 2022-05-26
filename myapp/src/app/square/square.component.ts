6853bb2f1f1f300de372243d9b988a5055f57066
sha26 <sha26@sfu.ca>
2022-05-26 15:44:04 -0700

    color scheme

--- myapp/src/app/square/square.component.ts
+++ myapp/src/app/square/square.component.ts
@@ -4,13 +4,14 @@ import { Component, Input } from '@angular/core';
   selector: 'app-square',
   template: `<button>{{ value }}</button>
   `,
-  styles:[]
+  styles:['button { width: 100%; height: 100%; font-size: 5em !important; background-color: #78D19E;border:none; }']
 })
 //   templateUrl: './square.component.html',
 //   styleUrls: ['./square.component.scss']
 // })
 export class SquareComponent  {
 
-  @Input() value: 'X' | 'O' | undefined;
+  @Input()
+  value: 'X' | 'O' = "X";
 
 }
